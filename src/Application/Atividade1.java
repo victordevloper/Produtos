d220d52feac46441295e4b2cf93a7c072fde8901
Victor <luanvitorlima2017@gmail.com>
2023-11-06 10:13:50 -0300

    saida de dados e primeira atividade

---
+++ src/Application/Atividade1.java
@@ -0,0 +1,35 @@
+package Application;
+
+import java.util.Locale;
+
+public class Atividade1
+{
+	public static void Executar()
+	{
+		String product1 = "Computer";
+		String product2 = "Office Desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'f';
+		
+		double price1 = 2100.00;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf(product1 + " which price is $" + price1 + "%n");
+		System.out.printf(product2 + " which price is $" + price2  + "%n");
+		
+		System.out.println("Record: " + age + " years old, code: " + code + " and gender: " + gender);
+		System.out.println("Measure with eight decimal places: " + measure);
+		System.out.print("Rouded (three decimal places): ");
+		System.out.printf("%.3f%n", measure);
+		
+		Locale.setDefault(Locale.US);
+		System.out.print("US decimal point: ");
+		System.out.printf("%.3f%n", measure);			
+		
+	}
+			
+}
---
+++ src/Application/SaidaDeDados.java
@@ -0,0 +1,15 @@
+package Application;
+
+import java.util.Locale;
+
+public class SaidaDeDados 
+{
+	public static void Execute()
+	{
+		double x = 10.35784;
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.printf("Resultado: %.2f metros %n ", x);
+		Locale.setDefault(Locale.US);
+	}
+}
