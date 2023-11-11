cabdf28b82ee3c0e9c39a769c52834eb3a5edf86
Victor <luanvitorlima2017@gmail.com>
2023-11-10 21:27:40 -0300

    continuação

--- src/Application/project6_ExpressoesLogicas.java
+++ src/Application/project6_ExpressoesLogicas.java
@@ -1,5 +1,7 @@
 package Application;
 
+import java.util.Scanner;
+
 public class project6_ExpressoesLogicas 
 {
 	public static void Executar()
@@ -14,5 +16,32 @@ public class project6_ExpressoesLogicas
 		{
 			System.out.println("Você não é o Victor");
 		}
+		
+		//expressão Logica pt. 2
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int hora;
+		
+		System.out.println("Que Horas são?: ");
+		hora = sc.nextInt();
+		
+		if(hora < 12)
+		{
+			System.out.println("Bom Dia.");
+		}
+		else
+		{
+			if(hora < 18)
+			{
+				System.out.println("Boa Tarde.");
+			}
+			else 
+			{
+				System.out.println("Boa Noite");
+			}
+		}
+		
+		sc.close();
 	}
 }
