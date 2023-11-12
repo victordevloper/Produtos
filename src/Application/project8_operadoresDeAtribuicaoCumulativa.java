aadd509c1f8c097bde06dcfd61946ec5a41e6ad3
Victor <luanvitorlima2017@gmail.com>
2023-11-11 23:25:47 -0300

    exemplificando

--- src/Application/project8_operadoresDeAtribuicaoCumulativa.java
+++ src/Application/project8_operadoresDeAtribuicaoCumulativa.java
@@ -14,14 +14,10 @@ public class project8_operadoresDeAtribuicaoCumulativa
 		
 		double conta = 50.0;
 		
-		if(minutos > 100)
-		{
-			conta = conta + (minutos - 100) * 2.0;
-		}
-		
-		System.out.printf("Valor da conta = R$ %.2f%n", conta);
-		
-		sc.close();
+		//if(minutos > 100)
+		//{
+		//	conta = conta + (minutos - 100) * 2.0;
+		//}
 		
 		
 		//operadores de atribuição acumulativa
@@ -31,6 +27,18 @@ public class project8_operadoresDeAtribuicaoCumulativa
 		// A *= B; mesmo que: A = A * B
 		// A /= B; mesmo que: A = A / B
 		// A %= B; mesmo que: A = A % B
+		
+		//simplificando o nosso programa, podemos fazer da seguinte forma:
+		
+		if(minutos > 100)
+		{
+			//atribuindo os valores a conta de forma simplificada com a atribuição cumulativa
+			conta += (minutos - 100) * 2.0;
+		}
+		
+		System.out.printf("Valor da conta = R$ %.2f%n", conta);
+		
+		sc.close();
 		
 	}
 }
