7e154d685be3fdbb2884fe43a420d5c24a325063
Victor <luanvitorlima2017@gmail.com>
2023-11-13 21:34:56 -0300

    estrutura for

--- src/EstruturasCondicionais/Program.java
+++ src/EstruturasCondicionais/Program.java
@@ -2,11 +2,12 @@ package EstruturasCondicionais;
 
 import EstruturasRepetitivas.project1_EstruturaRepetitivaWhile;
 import EstruturasRepetitivas.project2_Exercicio1;
+import EstruturasRepetitivas.project3_EstruturaRepetitivaFor;
 
 public class Program
 {	
 	public static void main(String[] args) 
 	{
-		project2_Exercicio1.Executar();
+		project3_EstruturaRepetitivaFor.Executar();
 	}
 }
---
+++ src/EstruturasRepetitivas/project3_EstruturaRepetitivaFor.java
@@ -0,0 +1,12 @@
+package EstruturasRepetitivas;
+
+public class project3_EstruturaRepetitivaFor 
+{
+	public static void Executar()
+	{
+		for(int x = 0; x <= 10; x++)
+		{
+			System.out.println("Mensagem");
+		}
+	}
+}
