ea58a513e406563d837eb644c088f625b022ab50
Victor <luanvitorlima2017@gmail.com>
2023-11-15 19:37:08 -0300

    verificação de bitwise

--- src/EstruturasCondicionais/Program.java
+++ src/EstruturasCondicionais/Program.java
@@ -3,11 +3,12 @@ package EstruturasCondicionais;
 import EstruturasRepetitivas.project1_EstruturaRepetitivaWhile;
 import EstruturasRepetitivas.project2_Exercicio1;
 import EstruturasRepetitivas.project3_EstruturaRepetitivaFor;
+import OutrosTopicos.VerificandoBitwise;
 
 public class Program
 {	
 	public static void main(String[] args) 
 	{
-		project3_EstruturaRepetitivaFor.Executar();
+		VerificandoBitwise.Executar();
 	}
 }
---
+++ src/OutrosTopicos/VerificandoBitwise.java
@@ -0,0 +1,23 @@
+package OutrosTopicos;
+
+import java.util.Scanner;
+
+public class VerificandoBitwise 
+{
+	public static void Executar()
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int mask = 0b0010000;
+		int n = sc.nextInt();
+		
+		if((n & mask) != 0)
+		{
+			System.out.println("6th bit is true");
+		}
+		else
+		{
+			System.out.println("6th bit is false");
+		}
+	}
+}
