c9339c6614f1651ca2030603a8ecc7055cc2fbe5
Victor <luanvitorlima2017@gmail.com>
2023-11-16 18:55:14 -0300

    funções com strings

--- src/EstruturasCondicionais/Program.java
+++ src/EstruturasCondicionais/Program.java
@@ -1,14 +1,12 @@
 package EstruturasCondicionais;
 
-import EstruturasRepetitivas.project1_EstruturaRepetitivaWhile;
-import EstruturasRepetitivas.project2_Exercicio1;
-import EstruturasRepetitivas.project3_EstruturaRepetitivaFor;
+import OutrosTopicos.FuncoesParaString;
 import OutrosTopicos.VerificandoBitwise;
 
 public class Program
 {	
 	public static void main(String[] args) 
 	{
-		VerificandoBitwise.Executar();
+		FuncoesParaString.Executar();
 	}
 }
